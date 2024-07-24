8da482e27e65ca6b6800f9be874062e0f6f9ff8c
sontv <sontv@MacBook-Pro-cua-sontv.local>
2024-07-24 13:09:17 +0700

    register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,8 @@
 import { registerSchema } from "../schemas/auth";
+import User from "../models/auth";
+import bcryptjs from "bcryptjs";
 
-export const signup = (request, response) => {
+export const signup = async (request, response) => {
   // lay du lieu user gui len
   //   console.log(request.body.password);
   const { username, email, password } = request.body;
@@ -15,11 +17,30 @@ export const signup = (request, response) => {
     response.status(400).json({ errorMessage: message });
   }
   // kiem tra user da ton tai hay chua
-  // ma hoa mat khau
+  const existUser = await User.findOne({ email: email });
+  if (existUser) {
+    return response.status(400).json({ errorMessage: "Email da ton tai" });
+  }
+  // ma hoa mat khau bcryptjs
+  const hashedPassword = await bcryptjs.hash(password, 10);
+  // console.log(hashedPassword);
   // them moi user vao db
+  const user = await User({
+    username,
+    email,
+    password: hashedPassword,
+  }).save();
   // tra ve thong tin user vua dang ky (khong bao gom mat khau)
+  response.status(201).json({ message: "Dang ky thanh cong", user });
 };
 
 export const signin = (request, response) => {
-  console.log("Dang nhap");
+  // console.log("Dang nhap");
+  // lay thong tin user gui len
+  // kiem tra tinh hop le cua thong tin
+  // kiem tra user co ton tai hay khong
+  // neu khong ton tai thi tra ve thong bao loi khong tim thay user
+  // neu ton tai thi kiem tra mat khau
+  // neu mat khau khong khop thi tra ve thong bao sai mat khau
+  // neu mat khau khop thi tra ve thong tin user
 };
